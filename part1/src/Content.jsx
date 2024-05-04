f09f898358ac86003c39c5ad3fa7a4a01630745b
Obed Issaí Tzay Puac <Ovejatzaypuac03@gmail.com>
2024-05-04 16:30:31 -0600

    exercises 1.3-1.5

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -1,9 +1,16 @@
 
 
 const Content =(props)=>{
+    console.log(props.course.parts);
+    const [first, second, third] = props.course.parts;
+
 
     return (
-        <p>{props.part} {props.exercises}</p>
+        <>
+           <p>{first.name} {first.exercises}</p>
+           <p>{second.name} {second.exercises}</p>
+           <p>{third.name} {third.exercises}</p>
+        </>
     )
 }
 
--- part1/src/Total.jsx
+++ part1/src/Total.jsx
@@ -2,8 +2,10 @@
 
 
 const Total=(props)=>{
+    console.log(props.course.parts);
+    const [first, second, third] = props.course.parts;
 
-    return <p>Number of exercises {props.suma}</p>
+    return <p>Number of exercises {first.exercises+second.exercises+third.exercises}</p>
 }
 
 export default Total
